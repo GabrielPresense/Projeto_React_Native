1b3f17f0dcdfa594a7f0b33af56778cdc21172fc
Gabriel Presense <gabrielpresense@gmail.com>
2025-08-07 09:06:48 -0300

    Correções

--- onebithealth/src/components/Form/index.js
+++ onebithealth/src/components/Form/index.js
@@ -49,12 +49,12 @@ export default function Form(props){
          setTextButton("Calcular")
          setMessageImc("preencha o peso e altura")
       }
-    
    }
 
     return(
-       <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
-         <View style={styles.form}>
+       <View style={styles.formContext}>
+         {imc == null ?
+       <Pressable onPress={Keyboard.dismiss} style={styles.form}>
             <Text style={styles.formLabel}>Altura</Text>
             <Text style={styles.errorMessage}>{errorMessage}</Text>
             <TextInput
@@ -79,8 +79,18 @@ export default function Form(props){
             >
                <Text style={styles.textButtonCalculator}>{textButton}</Text>
             </TouchableOpacity>
+         </Pressable>
+         : 
+         <View style={styles.exhibitionResultImc}>
+            <ResultImc messageResultIMc={messageImc} resultImc={imc} />
+            <TouchableOpacity
+               style={styles.buttonCalculator}
+               onPress={() =>{validationImc()}}
+            >
+               <Text style={styles.textButtonCalculator}>{textButton}</Text>
+            </TouchableOpacity>
          </View>
-         <ResultImc messageResultImc={messageImc} resultImc={imc} />
-       </Pressable>
+         }
+       </View>
     );
 }
