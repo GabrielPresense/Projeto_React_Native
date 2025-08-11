75d15a5979ea0fc994289d0f17bacc06eee2678e
Gabriel Presense <gabrielpresense@gmail.com>
2025-08-11 16:14:11 -0300

    Inicio do projeto OneBitCoin

--- OneBitCoin/App.js
+++ OneBitCoin/App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, StatusBar, SafeAreaView, Platform } from 'react-native';
+
+import CurrentPrice from './src/components/CurrentPrice';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+      backgroundColor="#f50d41"
+      barStyle="light-content"
+      />
+      <CurrentPrice/>
     </SafeAreaView>
   );
 }
@@ -16,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
     alignItems: 'center',
-    paddingTop: Plataform.OS === "android" ? 40: 0
+    paddingTop: Platform.OS === "android" ? 40: 0
   },
 });
